101b611e6d3b09f4ef8436e4899dea11a930bb96
yakovypg <yasha-pmp@mail.ru>
2025-07-09 16:01:17 +0300

    Add localization to cards and mistakes pages

--- src/panels/Cards.tsx
+++ src/panels/Cards.tsx
@@ -1,28 +1,36 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 import {
   NavIdProps,
   Panel,
   PanelHeader,
-  PanelHeaderBack,
-  Placeholder,
+  PanelHeaderBack
 } from "@vkontakte/vkui";
 
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
+import { changeLanguage } from "../utils/i18n";
 
 export const Cards: FC<NavIdProps> = ({ id }) => {
+  const { t } = useTranslation();
   const routeNavigator = useRouteNavigator();
 
   return (
     <Panel id={id}>
       <PanelHeader
         before={<PanelHeaderBack onClick={() => routeNavigator.back()} />}
+        after={
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-sm mx-2"
+            onClick={changeLanguage}
+          >
+            {t("shortName")}
+          </button>
+        }
       >
-        Карточки
+        {t("title.cards")}
       </PanelHeader>
-      <Placeholder>
-        <p>Карточки</p>
-      </Placeholder>
     </Panel>
   );
 };
--- src/panels/Mistakes.tsx
+++ src/panels/Mistakes.tsx
@@ -1,26 +1,36 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
+
 import {
   NavIdProps,
   Panel,
   PanelHeader,
-  PanelHeaderBack,
-  Placeholder,
+  PanelHeaderBack
 } from "@vkontakte/vkui";
+
 import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
+import { changeLanguage } from "../utils/i18n";
 
 export const Mistakes: FC<NavIdProps> = ({ id }) => {
+  const { t } = useTranslation();
   const routeNavigator = useRouteNavigator();
 
   return (
     <Panel id={id}>
       <PanelHeader
         before={<PanelHeaderBack onClick={() => routeNavigator.back()} />}
+        after={
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-sm mx-2"
+            onClick={changeLanguage}
+          >
+            {t("shortName")}
+          </button>
+        }
       >
-        Работа над ошибками
+        {t("title.workOnMistakes")}
       </PanelHeader>
-      <Placeholder>
-        <p>Работа над ошибками</p>
-      </Placeholder>
     </Panel>
   );
 };
