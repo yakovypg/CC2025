5786464fa7f3b9911c04985857875831f24b2abe
yakovypg <yasha-pmp@mail.ru>
2025-07-09 14:30:06 +0300

    Fix mistake in type export

--- src/panels/index.ts
+++ src/panels/index.ts
@@ -1,6 +1,6 @@
-export { Home } from './Home';
-export { Cards } from './Cards';
-export { Mistakes } from './Mistakes';
-export { Profile } from './Profile';
+export { Home } from "./Home";
+export { Cards } from "./Cards";
+export { Mistakes } from "./Mistakes";
+export { Profile } from "./Profile";
 
-export type { HomeProps } from './Home';
+export type { ProfileProps } from "./Profile";
