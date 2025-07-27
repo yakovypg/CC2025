b6b7809dbe60f23d1669ca31cf0bd892a11b7d6c
yakovypg <yasha-pmp@mail.ru>
2025-07-27 08:57:40 +0300

    Migrate to eslint.config.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,46 @@
+const { defineConfig, globalIgnores } = require("eslint/config");
+const { fixupConfigRules } = require("@eslint/compat");
+
+const globals = require("globals");
+
+const reactRefresh = require("eslint-plugin-react-refresh");
+const tsParser = require("@typescript-eslint/parser");
+const js = require("@eslint/js");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+
+      parser: tsParser
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:import/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+      )
+    ),
+
+    plugins: {
+      "react-refresh": reactRefresh
+    },
+
+    rules: {}
+  },
+  globalIgnores(["**/dist", "**/.eslintrc.cjs"])
+]);
