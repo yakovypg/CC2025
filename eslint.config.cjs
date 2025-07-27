179e7af4c521f0fc52129017556963710795ee0f
yakovypg <yasha-pmp@mail.ru>
2025-07-27 11:15:19 +0300

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -54,6 +54,8 @@ module.exports = defineConfig([
     },
 
     rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
       "import/order": [
         "error",
         {
