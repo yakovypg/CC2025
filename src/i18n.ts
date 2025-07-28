d110c6642b7c94f4f217ca880d3b33073e84e9c7
yakovypg <yasha-pmp@mail.ru>
2025-07-28 11:55:50 +0300

    Add language detection for i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
   .use(HttpBackend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     supportedLngs: ["en", "ru"],
