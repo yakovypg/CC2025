bb5adbe3651907efbcfdc137dbc83b4197ae55d4
yakovypg <yasha-pmp@mail.ru>
2025-07-30 14:34:27 +0300

    Update default locale config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,22 @@
+import { EGetLaunchParamsResponseLanguages } from "@vkontakte/vk-bridge";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+
+import { getLaunchLanguage } from "./config";
+
+const launchLanguage: EGetLaunchParamsResponseLanguages =
+  getLaunchLanguage() ?? EGetLaunchParamsResponseLanguages.EN;
 
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    supportedLngs: ["en", "ru"],
-    lng: "en",
-    fallbackLng: "en",
+    supportedLngs: [EGetLaunchParamsResponseLanguages.EN, EGetLaunchParamsResponseLanguages.RU],
+    lng: launchLanguage,
+    fallbackLng: EGetLaunchParamsResponseLanguages.EN,
     interpolation: { escapeValue: false },
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json"
