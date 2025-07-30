b8ee77f25aac5a3130565e5949d2991f4307ec06
yakovypg <yasha-pmp@mail.ru>
2025-07-30 13:40:27 +0300

    Refactor code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { getErrorMessage, ErrorType } from "./errorMessage";
 export { changeLanguage } from "./i18n";
-export { isPageReloaded } from "./reloadCheck"
+export { isPageReloaded } from "./reloadCheck";
 export { transformVkBridgeAdaptivity } from "./transformVkBridgeAdaptivity";
 export { TEST_USER as testUser } from "./user";
