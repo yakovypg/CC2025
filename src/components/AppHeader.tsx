e5d2817844bf54005d0d91bec82f138b281a1283
yakovypg <yasha-pmp@mail.ru>
2025-07-30 13:11:02 +0300

    Fix display on mobile platform

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,6 +3,7 @@ import { PanelHeader, PanelHeaderBack, PanelHeaderClose } from "@vkontakte/vkui"
 import { FC, ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 
+import { isOnMobilePlatform } from "../config";
 import { AppHeaderButtonType } from "../types";
 import { changeLanguage } from "../utils";
 
@@ -18,7 +19,7 @@ export const AppHeader: FC<AppHeaderProps> = ({ title, buttonType }: AppHeaderPr
   const { t } = useTranslation();
   const routeNavigator: RouteNavigator = useRouteNavigator();
 
-  const button: ReactNode =
+  const navigationButton: ReactNode =
     buttonType === AppHeaderButtonType.BACK ? (
       <PanelHeaderBack onClick={() => routeNavigator.back()} />
     ) : buttonType === AppHeaderButtonType.CLOSE ? (
@@ -27,17 +28,30 @@ export const AppHeader: FC<AppHeaderProps> = ({ title, buttonType }: AppHeaderPr
       <PanelHeaderClose onClick={() => routeNavigator.back()} />
     ) : undefined;
 
+  const languageButton: ReactNode = (
+    <button
+      type="button"
+      className="btn btn-outline-secondary btn-sm mx-2"
+      onClick={changeLanguage}>
+      {t("shortName")}
+    </button>
+  );
+
+  const alignLanguageButtonRight: boolean = !isOnMobilePlatform();
+
+  const panelHeaderBefore: ReactNode = alignLanguageButtonRight ? (
+    navigationButton
+  ) : (
+    <div className="d-flex align-items-center">
+      {navigationButton}
+      <div className="ml-2">{languageButton}</div>
+    </div>
+  );
+
+  const panelHeaderAfter: ReactNode = alignLanguageButtonRight ? languageButton : undefined;
+
   return (
-    <PanelHeader
-      before={button}
-      after={
-        <button
-          type="button"
-          className="btn btn-outline-secondary btn-sm mx-2"
-          onClick={changeLanguage}>
-          {t("shortName")}
-        </button>
-      }>
+    <PanelHeader before={panelHeaderBefore} after={panelHeaderAfter}>
       <div className="d-flex align-items-center justify-content-center">
         <p className="fs-6 fw-bold mb-0 centered-entire-width">{title}</p>
       </div>
